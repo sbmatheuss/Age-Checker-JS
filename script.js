477eec7c815180b89b29d349b4404dacf4fe5cc5
Matheus Braga <sbmatheus20@gmail.com>
2025-09-26 23:27:55 -0300

    referência total aos elementos da página

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Cria referência total aos elementos da página.
+const anoNascimeto = document.querySelector('input#anoNascimento') 
+const verificarBtn = document.querySelector('button#verificarBtn')
+const resultadoDiv = document.querySelector('div#resultado')
+
